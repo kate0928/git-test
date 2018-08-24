224b417ae67873be394b577854f8f1f2cf7dc8e8
kate <tesuyo0928@gmail.com>
2018-08-24 23:39:38 +0800

    test

--- src/my-view1.js
+++ src/my-view1.js
@@ -24,7 +24,7 @@ class MyView1 extends PolymerElement {
 
       <div class="card">
         <div class="circle">1</div>
-        測試網頁
+        這是一個練習測試網頁
         <h1>View One</h1>
         <p>Ut labores minimum atomorum pro. Laudem tibique ut has.</p>
         <p>Lorem ipsum dolor sit amet, per in nusquam nominavi periculis, sit elit oportere ea.Lorem ipsum dolor sit amet, per in nusquam nominavi periculis, sit elit oportere ea.Cu mei vide viris gloriatur, at populo eripuit sit.</p>
